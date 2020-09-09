8bb8fefb3f43b48b9a52ebe036c0f8be751d9dea
Trangar <victor.koenders@gmail.com>
2020-09-09 19:49:01 +0200

    Fixed compiler error, updated to mikro-orm 4.0.0

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,9 +1,9 @@
-import { AnyEntity, Entity, PrimaryKey, Property, OneToMany, Collection } from 'mikro-orm';
+import { Entity, PrimaryKey, Property, OneToMany, Collection } from 'mikro-orm';
 import { Payment } from './payment';
 import { v4 } from 'uuid';
 
 @Entity()
-export class User implements AnyEntity<User, 'id'> {
+export class User {
 	@PrimaryKey()
 	id: string;
 
